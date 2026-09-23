4f133b186240160622e818b572c1f814f586d804
moonbyulsimp <diannewhales@gmail.com>
2026-09-23 17:58:43 +0800

    Add 10% group discount for orders of 5+ tickets - sudueste.preciousdianne

--- tickets.js
+++ tickets.js
@@ -3,7 +3,13 @@ function isValidQuantity(quantity) {
 }
 
 function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+  let price = quantity * basePrice;
+
+  if (quantity >= 5) {
+    price *= 0.90;
+  }
+
+  return Math.floor(price);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };

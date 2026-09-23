43235ec14629165e9b6c4808f6a43bd7d2d31937
moonbyulsimp <diannewhales@gmail.com>
2026-09-23 18:29:12 +0800

    Add 50% VIP surcharge for premium seating - sudueste.preciousdianne

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, seatingType) {
+  let price = quantity * basePrice;
+
+  if (seatingType === "premium") {
+    price = price * 1.50;
+  }
+
+  return Math.round(price);
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
